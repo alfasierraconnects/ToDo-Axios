1e3f43410c8d7fcca2c94ebd98efb8edf1d1d6f9
alfasierraconnects <alfasierraconnects@gmail.com>
2024-03-05 19:58:02 +0530

    added document

---
+++ index.js
@@ -0,0 +1,25 @@
+const getBtn = document.getElementById("get-btn");
+const postBtn = document.getElementById("post-btn");
+const putBtn = document.getElementById("put-btn");
+const deleteBtn = document.getElementById("delete-btn");
+
+getBtn.addEventListener("click", getTodos);
+postBtn.addEventListener("click", postTodo);
+putBtn.addEventListener("click", putTodo);
+deleteBtn.addEventListener("click", deleteTodo);
+
+function getTodos() {
+  // Write your code here
+}
+
+function postTodo() {
+  // Write your code here
+}
+
+function putTodo() {
+  // Write your code here
+}
+
+function deleteTodo() {
+  // Write your code here
+}
